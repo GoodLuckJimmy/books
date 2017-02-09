87415a0e74d9261398927122ab2ae41830a034ba
seok.han <seok.han@vovioskorea.com>
2017-02-09 14:26:12 +0900

    ch4

---
+++ HF OOA&D/src/headfirst/ooa/ch4/Bark.java
@@ -0,0 +1,26 @@
+package headfirst.ooa.ch4;
+
+public class Bark {
+	
+	private String sound;
+	
+	public Bark(String sound) {
+		this.sound = sound;
+	}
+	
+	public String getSound() {
+		return sound;
+	}
+	
+	public boolean equals(Object bark) {
+		if (bark instanceof Bark) {
+			Bark otherBark = (Bark) bark;
+			
+			if (this.sound.equalsIgnoreCase(otherBark.sound)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
