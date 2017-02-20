ae749454c1501e79267afda5272b742b99e1d427
seok.han <seok.han@vovioskorea.com>
2017-02-20 11:40:52 +0900

    appendix1

---
+++ HF OOA&D/src/headfirst/ooa/appendix1/Airplane.java
@@ -0,0 +1,18 @@
+package headfirst.ooa.appendix1;
+
+public class Airplane {
+	
+	public int speed;
+	
+	public Airplane() {
+	}
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+
+}
