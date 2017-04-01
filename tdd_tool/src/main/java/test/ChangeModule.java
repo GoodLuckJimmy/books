a9ce8256a37a5be29386159ba2319ae578b40ff2
HAN <HAN@192.168.0.2>
2017-04-01 20:11:28 +0900

    module added

--- tdd_tool/src/main/java/test/ChangeModule.java
+++ tdd_tool/src/main/java/test/ChangeModule.java
@@ -1,31 +1,36 @@
 package test;
 
 public class ChangeModule {
+	enum COIN {
+		KRW500(500), KRW100(100), KRW50(50), KRW10(10);
+		private int value;
+		COIN(int value) {
+			this.value = value;
+		}
+	}
 
 	public CoinSet getChangeCoinSet(int changeAmount) {
 		CoinSet coinSet = new CoinSet();
 		
-		while(changeAmount >= 500) {
-			changeAmount -= 500;
-			coinSet.add(500);
-		}
-		
-		while(changeAmount >= 100) {
-			changeAmount -= 100;
-			coinSet.add(100);
-		}
-		
-		while(changeAmount >= 50) {
-			changeAmount -= 50;
-			coinSet.add(50);
-		}
-		
-		while(changeAmount >= 10) {
-			changeAmount -= 10;
-			coinSet.add(10);
-		}
+		final int KRW500 = COIN.KRW500.value;
+		final int KRW100 = 100;
+		final int KRW50 = 50;
+		final int KRW10 = 10;
+
+		changeAmount = addCoinsToCoinSet(changeAmount, coinSet, KRW500);
+		changeAmount = addCoinsToCoinSet(changeAmount, coinSet, KRW100);
+		changeAmount = addCoinsToCoinSet(changeAmount, coinSet, KRW50);
+		changeAmount = addCoinsToCoinSet(changeAmount, coinSet, KRW10);
 
 		return coinSet;
+	}
+
+	private int addCoinsToCoinSet(int changeAmount, CoinSet coinSet, int KRW500) {
+		while(changeAmount >= KRW500) {
+			changeAmount -= KRW500;
+			coinSet.add(KRW500);
+		}
+		return changeAmount;
 	}
 
 }
