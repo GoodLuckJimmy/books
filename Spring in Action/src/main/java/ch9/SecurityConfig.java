1d77bb5ebee5c6905694f096b55af7072163be07
HAN <HAN@192.168.0.2>
2017-04-03 20:39:41 +0900

    spring in action

---
+++ Spring in Action/src/main/java/ch9/SecurityConfig.java
@@ -0,0 +1,11 @@
+package ch9;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
