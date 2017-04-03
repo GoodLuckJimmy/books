234ec57392cea4f60a0b4cc14db5574868f743ef
HAN <HAN@192.168.0.2>
2017-04-03 21:13:31 +0900

    p286

--- Spring in Action/src/main/java/ch9/SecurityConfig.java
+++ Spring in Action/src/main/java/ch9/SecurityConfig.java
@@ -1,6 +1,8 @@
 package ch9;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -13,7 +15,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		  .authorizeRequests()
-			.anyRequest().authenticated()
-			.and().formLogin();
+		  .antMatchers("/spitters/me").authenticated()
+		  .antMatchers(HttpMethod.POST, "/spittles").authenticated()
+		  .anyRequest().permitAll();
+	}
+
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and()
+		.withUser("admin").password("password").roles("USER", "ADMIN");
 	}
 }
