60fd2f624773c56531a93b85b4dd354b0ac27fa1
HAN <HAN@192.168.0.2>
2017-04-12 22:24:04 +0900

    p43

---
+++ ibmoon/src/main/java/ibmoon/Aggregate.java
@@ -0,0 +1,6 @@
+package ibmoon;
+
+public interface Aggregate {
+	public abstract Iterator iterator();
+
+}
---
+++ ibmoon/src/main/java/ibmoon/Iterator.java
@@ -0,0 +1,7 @@
+package ibmoon;
+
+public interface Iterator {
+	public abstract boolean hasNext();
+	public abstract Object next();
+
+}
