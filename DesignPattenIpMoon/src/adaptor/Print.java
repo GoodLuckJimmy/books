af2ab71828dcdbb5db7e23fda41a823fe72092d9
HAN <HAN@192.168.0.2>
2017-04-14 23:15:02 +0900

    p59

---
+++ DesignPattenIpMoon/src/adaptor/Print.java
@@ -0,0 +1,8 @@
+package adaptor;
+
+public interface Print {
+	
+	public abstract void printWeak();
+	public abstract void printStrong();
+
+}
---
+++ DesignPattenIpMoon/src/adaptor/PrintBanner.java
@@ -0,0 +1,19 @@
+package adaptor;
+
+public class PrintBanner extends Banner implements Print {
+	
+	public PrintBanner(String string) {
+		super(string);
+	}
+
+	@Override
+	public void printWeak() {
+		showWithParen();
+	}
+
+	@Override
+	public void printStrong() {
+		showWithAster();
+	}
+
+}
