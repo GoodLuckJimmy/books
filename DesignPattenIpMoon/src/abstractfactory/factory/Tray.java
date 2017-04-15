fa6d8c8d61e74ca1eb459f112e06899795d4d120
HAN <HAN@192.168.0.2>
2017-04-16 01:13:23 +0900

    p143

---
+++ DesignPattenIpMoon/src/abstractfactory/factory/Tray.java
@@ -0,0 +1,16 @@
+package abstractfactory.factory;
+
+import java.util.ArrayList;
+
+public abstract class Tray extends Item {
+	protected ArrayList<Item> tray = new ArrayList<>();
+
+	public Tray(String caption) {
+		super(caption);
+	}
+
+	public void add(Item item) {
+		tray.add(item);
+	}
+
+}
