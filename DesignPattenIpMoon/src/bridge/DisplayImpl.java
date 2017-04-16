9f5a4b8721840b7c84f84a325ed64c4515c84b14
HAN <HAN@192.168.0.2>
2017-04-16 22:54:52 +0900

    p176

---
+++ DesignPattenIpMoon/src/bridge/DisplayImpl.java
@@ -0,0 +1,9 @@
+package bridge;
+
+// Implementor(구현자)의 역할
+public abstract class DisplayImpl {
+	public abstract void rawOpen();
+	public abstract void rawPrint();
+	public abstract void rawClose();
+
+}
