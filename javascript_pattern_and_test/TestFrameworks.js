2ee462bebf5304743d2f4810fbfc43918abe217b
seokhan <bakgaboon@gmail.com>
2018-04-04 20:12:19 +0900

    p94

--- javascript_pattern_and_test/TestFrameworks.js
+++ javascript_pattern_and_test/TestFrameworks.js
@@ -1,6 +1,92 @@
-function createReservation(passenger, flight) {
-    return {
+function createReservation(passenger, flight, saver) {
+    var reservation = {
         passengerInfo: passenger,
         flightInfo: flight
     };
+
+    saver.saveReservation(reservation);
+    return reservation;
 }
+
+function ReservationSaver() {
+    this.saveReservation = function(reservation) {
+        // 원격 작업...
+    };
+}
+
+/**********************************************************/
+
+
+Attendee = function(service, messager, attendeeId) {
+    if (!(this instanceof Attendee)) {
+        return new Attendee(attendeeId);
+    }
+
+    this.attendeeId = attendeeId;
+    this.service = service;
+    this.message = messager;
+
+    // 주어진 세션에 좌석 예약을 시도한다.
+    // 성공/실패 여부를 메시지로 알려준다.
+    Attendee.prototype.reserve = function(sessionId) {
+        if (this.service.reserve(this.attendeeId, sessionId)) {
+            this.message.success('좌석 예약이 완료되었습니다.' +
+                '고객님은 ' + this.service.getRemainingReservations() +
+                ' 좌석을 추가 예약하실 수 있습니다.');
+        } else {
+            this.message.failure('죄송합니다. 해당 좌석은 예약하실 수 없습니다.');
+        }
+    };
+};
+
+
+/**********************************************************/
+
+DiContainer = function() {
+    if (!(this instanceof DiContainer)) {
+        return new DiContainer();
+    }
+    this.registrations = [];
+};
+
+DiContainer.prototype.message = {
+    registerRequiresArgs: '이 생성자 함수는 인자가 3개 있어야 합니다: ' +
+        '문자열, 문자열 배열, 함수.'
+};
+
+DiContainer.prototype.register = function(name, dependencies, func) {
+    var ix;
+
+    if (typeof name !=='string'
+        || !Array.isArray(dependencies)
+        || typeof func !== 'function') {
+        throw new Error(this.message.registerRequiresArgs);
+    }
+
+    for (ix=0; ix < dependencies.length; ++ix) {
+        if (typeof dependencies[ix] !== 'string') {
+            throw new Error(this.message.registerRequiresArgs);
+        }
+    }
+
+    this.registrations[name] = {
+        dependencies: dependencies,
+        func: func
+    };
+};
+
+DiContainer.prototype.get = function(name) {
+    var self = this;
+    var registration = this.registrations[name];
+    var dependencies = [];
+
+    if (registration === undefined) {
+        return undefined;
+    }
+
+    registration.dependencies.forEach(function(dependencyName) {
+        var dependency = self.get(dependencyName);
+        dependencies.push(dependency === undefined ? undefined : dependency);
+    });
+    return registration.func.apply(undefined, dependencies);
+};
--- javascript_pattern_and_test/TestFrameworks_test.js
+++ javascript_pattern_and_test/TestFrameworks_test.js
@@ -1,33 +1,112 @@
+/**********************************************************/
 describe('createReservation(passenger, flight)', function() {
-    it('주어진 passenger를 passengerInfo 프로퍼티에 할당한다.', function() {
-        var testPassenger = {
+    var testPassenger = null;
+    var testFlight = null;
+    var testReservation = null;
+    var testSaver = null;
+
+    beforeEach(function() {
+        testPassenger = {
             firstName: '윤지',
             lastName: '김'
-        };
+        }; 
 
-        var testFlight = {
+        testFlight = {
             number: '3443',
             carrier:'대한항공',
             destination: '울산'
         };
 
-        var reservation = createReservation(testPassenger, testFlight);
-        expect(reservation.passengerInfo).toBe(testPassenger);
+        testSaver = new ReservationSaver();
+        spyOn(testSaver, 'saveReservation');
+
+        testReservation = createReservation(testPassenger, testFlight, testSaver);
+    });
+
+    it('주어진 passenger를 passengerInfo 프로퍼티에 할당한다.', function() {
+        expect(testReservation.passengerInfo).toBe(testPassenger);
     });
 
     it('주어진 flight를 flightInfo 프로퍼티에 할당한다.', function() {
-        var testPassenger = {
-            firstName: '윤지',
-            lastName: '김'
-        };
+        expect(testReservation.flightInfo).toBe(testFlight);
+    });
 
-        var testFlight = {
-            number: '3443',
-            carrier:'대한항공',
-            destination: '울산'
-        };
+    it('예약 정보를 저장한다.', function() {
+        expect(testSaver.saveReservation).toHaveBeenCalled();
+    });
+});
 
-        var reservation = createReservation(testPassenger, testFlight);
-        expect(reservation.flightInfo).toBe(testFlight);
+/**********************************************************/
+describe('DiContainer', function() {
+    var container;
+
+    beforeEach(function() {
+        container = new DiContainer();
+    });
+
+    describe('register(name, dependencies, func)', function() {
+        it('인자가 하나라도 빠졌거나 타입이 잘못되면 예외를 던진다.', function() {
+            var badArgs = [
+                [], // 인자가 아예 없는 경우
+                ['Name'], // name만 있는경우
+                ['Name', ['Dependency1', 'Dependency2']], // name과 dependency만 있는경우
+                ['Name', function() {}], // dependencies가 빠진경우
+                // 타입이 잘못된 경우들
+                [1,['a','b'], function(){}],
+                ['Name',[1, 2], function(){}],
+                ['Name',['a', 'b'], 'should be a function']
+            ];
+
+            badArgs.forEach(function(args) {
+                expect(function() {
+                    container.register.apply(container, args);
+                }).toThrowError(container.message.registerRequiresArgs);
+            });
+        });
+    });
+
+    describe('get(name)', function() {
+        it('성명이 등록되어 있지 않으면 undefined를 반환한다.', function() {
+            expect(container.get('notDefined')).toBeUndefined();
+        });
+
+        it('등록된 함수를 실행한 결과를 반환한다', function() {
+            var name = 'MyName';
+            var returnFromRegisteredFunction = "something";
+
+            container.register(name, [], function() {
+                return returnFromRegisteredFunction;
+            });
+
+            expect(container.get(name)).toBe(returnFromRegisteredFunction);
+        });
+
+        it('등록된 함수에 의존성을 제공한다.', function() {
+            var main = 'main';
+            var mainFunc;
+            var dep1 = 'dep1';
+            var dep2 = 'dep2';
+
+            container.register(dep1, [], function() {
+                return function() {
+                    return 1;
+                };
+            });
+
+            container.register(dep2, [], function() {
+                return function() {
+                    return 2;
+                }
+            });
+
+            container.register(main, [dep1, dep2], function(dep1Func, dep2Func) {
+                return function() {
+                    return dep1Func() + dep2Func();
+                };
+            });
+
+            mainFunc = container.get(main);
+            expect(mainFunc()).toBe(3);
+        });
     });
 });
